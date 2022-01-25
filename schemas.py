2d0e487c278c98d10f1adecc488b685632aaf201
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-01-25 22:50:37 +0100

    refactored schemas

--- schemas.py
+++ schemas.py
@@ -3,5 +3,5 @@ from . import models
 
 ShoppingListPluginUnit = pydantic_model_creator(models.ShoppingListPluginUnit, name="ShoppingListPluginUnit")
 
-ShoppingListPluginUnitIn = pydantic_model_creator(models.ShoppingListPluginUnit, name="ShoppingListPluginUnit",
+ShoppingListPluginUnitIn = pydantic_model_creator(models.ShoppingListPluginUnit, name="ShoppingListPluginUnitIn",
                                                   exclude_readonly=True)
