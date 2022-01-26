c2ff4f3ad187472e5db565251ce122da615ca76f
Fabian Petersen <fabian.petersen@stud.hs-flensburg.de>
2022-01-26 11:43:53 +0100

    added a few schemas and updated the ListEntry Model

--- models.py
+++ models.py
@@ -15,16 +15,17 @@ class ShoppingListPluginUnit(Model):
 
 
 class ShoppingListPluginProduct(ShoppingListPluginDBBaseModel, Model):
-    name = fields.CharField(50, unique=True)
+    name = fields.CharField(50)
     unit_type = fields.ForeignKeyField("models.ShoppingListPluginUnit", related_name="shoppingListPluginProduct")
     creator = fields.ForeignKeyField("models.User", related_name="shoppingListPluginProduct")
 
 
 class ShoppingListPluginList(ShoppingListPluginDBBaseModel, Model):
+    name = fields.CharField(128)
     creator = fields.ForeignKeyField("models.User", related_name="shoppingListPluginList")
 
 
-class ShoppingListPluginListEntry(Model):
+class ShoppingListPluginListEntry(ShoppingListPluginDBBaseModel, Model):
     shoppinglist = fields.ForeignKeyField("models.ShoppingListPluginList", related_name="shoppingListPluginListEntry")
     product = fields.ForeignKeyField("models.ShoppingListPluginProduct", related_name="shoppingListPluginListEntry")
     amount = fields.FloatField()
--- schemas.py
+++ schemas.py
@@ -1,5 +1,5 @@
 import datetime
-from typing import Optional
+from typing import Optional, List
 from uuid import UUID
 
 from pydantic import BaseModel, Extra
@@ -32,10 +32,10 @@ class ShoppingListPluginProductPut(BaseModel):
 
 class ShoppingListPluginProductOut(BaseModel):
     uuid: UUID
-    name: str
-    unit_id: int
     created_at: datetime.datetime
     updated_at: datetime.datetime
+    name: str
+    unit_type: str
 
     class Config:
         extra = Extra.forbid
@@ -44,8 +44,85 @@ class ShoppingListPluginProductOut(BaseModel):
     async def from_model(cls, model: models.ShoppingListPluginProduct):
         return ShoppingListPluginProductOut(
             uuid=model.uuid,
-            name=model.name,
-            unit_id=(await model.unit_type).id,
             created_at=model.created_at,
-            updated_at=model.updated_at
+            updated_at=model.updated_at,
+            name=model.name,
+            unit_type=(await model.unit_type).unit,
+
+        )
+
+
+ShoppingListPluginListEntry = pydantic_model_creator(models.ShoppingListPluginListEntry,
+                                                     name="ShoppingListPluginListEntry")
+
+
+class ShoppingListPluginListEntryIn(BaseModel):
+    product_uuid: UUID
+    amount: float
+
+    class Config:
+        extra = Extra.forbid
+
+
+class ShoppingListPluginListEntryOut(BaseModel):
+    uuid: UUID
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    product_uuid: UUID
+    product_name: str
+    product_unit_type: int
+    amount: float
+
+    class Config:
+        extra = Extra.forbid
+
+    @classmethod
+    async def from_model(cls, model: models.ShoppingListPluginListEntry):
+        product: models.ShoppingListPluginProduct = await model.product
+        return ShoppingListPluginListEntryOut(
+            uuid=model.uuid,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            product_uuid=product.uuid,
+            product_name=product.name,
+            product_unit_type=(await product.unit_type).unit,
+            amount=model.amount
+
+        )
+
+
+class ShoppingListPluginListEntryPut(BaseModel):
+    product_uuid: Optional[UUID]
+    amount: Optional[float]
+
+    class Config:
+        extra = Extra.forbid
+
+
+ShoppingListPluginList = pydantic_model_creator(models.ShoppingListPluginList, name="ShoppingListPluginList")
+ShoppingListPluginListIn = pydantic_model_creator(models.ShoppingListPluginList, name="ShoppingListPluginListIn",
+                                                  exclude_readonly=True)
+
+
+class ShoppingListPluginListOut(BaseModel):
+    uuid: UUID
+    created_at: datetime.datetime
+    updated_at: datetime.datetime
+    name: str
+    entries: List[ShoppingListPluginListEntryOut]
+
+    class Config:
+        extra = Extra.forbid
+
+    @classmethod
+    async def from_model(cls, model: models.ShoppingListPluginList):
+        entries = []
+        async for entry in models.ShoppingListPluginListEntry.filter(shoppinglist=model):
+            entries.append(await ShoppingListPluginListEntryOut.from_model(entry))
+        return ShoppingListPluginListOut(
+            uuid=model.uuid,
+            created_at=model.created_at,
+            updated_at=model.updated_at,
+            name=model.name,
+            entries=entries
         )
